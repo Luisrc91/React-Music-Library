aa789d9101d0cc228471d0565bb9b1c7d527a9b3
Luis <luisrc91@live.com>
2023-03-28 09:06:02 -0400

    exploring-react-context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import { DataContext } from './context/DataContext'
+
 
 function App(){
     let [search, setSearch] = useState('')
     let [message, setMessage] = useState('Search for Music!')
     let [data, setData] = useState([])
-
+   
+  
     useEffect(() => {
       const fetchData = async () => {
           document.title = `${search} Music`
@@ -20,14 +23,31 @@ function App(){
       }
       fetchData()
   }, [search])
-  
+  const handleSearch = (e, term) => {
+    e.preventDefault();
+    const fetchData = async () => {
+      document.title = `${term} Music`;
+      const response = await fetch(
+        `https://itunes.apple.com/search?term=${term}`
+      );
+      const resData = await response.json();
+      if (resData.results.length > 0) {
+        return setData(resData.results);
+      } else {
+        return setMessage("Not Found.");
+      }
+    };
+    fetchData();
+  };
 
-    return (
-        <div>
-            <SearchBar />
-            {message}
+  return (
+    <div className="App">
+        <SearchBar handleSearch={handleSearch} />
+        {message}
+        <DataContext.Provider value={data} >
             <Gallery />
-        </div>
+        </DataContext.Provider>
+    </div>
     )
 }
 
